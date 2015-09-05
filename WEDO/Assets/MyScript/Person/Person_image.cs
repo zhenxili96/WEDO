0d3bd042d2205e2e86416161e5489020ae2a904c
S <821465068@qq.com>
2015-09-05 12:43:02 +0800

    commit before new client code enter

---
+++ WEDO/Assets/MyScript/Person/Person_image.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Person_image : MonoBehaviour
+{
+
+    public bool isHover = false;
+    public Vector3 originScale;
+    public Vector3 hoverScale;
+    public float scaleRate = 1.1f;
+    public float originZ;
+    public float hoverZ;
+
+    // Use this for initialization
+    void Start()
+    {
+        originScale = transform.localScale;
+        hoverScale = scaleRate * originScale;
+        originZ = transform.position.z;
+        hoverZ = originZ - 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkHover();
+        checkClick();
+        checkImage();
+    }
+
+    private void checkImage()
+    {
+        if (!Person_userimage.chooseUserImage.Equals(""))
+        {
+            string imagePath = "UserImage/" + Person_userimage.chooseUserImage;
+            renderer.material = (Material)Resources.Load(imagePath);
+        }
+        else
+        {
+            string imagePath = "UserImage/userimage0";
+            renderer.material = (Material)Resources.Load(imagePath);
+        }
+    }
+
+    private void checkClick()
+    {
+        if (isHover)
+        {
+            if (LeftHandProperty.isClosed && !LeftHandProperty.clickUsed)
+            {
+                transform.GetChild(0).gameObject.SetActive(true);
+                LeftHandProperty.clickUsed = true;
+            }
+            if (RightHandProperty.isClosed && !RightHandProperty.clickUsed)
+            {
+                transform.GetChild(0).gameObject.SetActive(true);
+                RightHandProperty.clickUsed = true;
+            }
+        }
+    }
+
+    private void checkHover()
+    {
+        if (RayHit.LeftHitName.Equals(name) || RayHit.RightHitName.Equals(name))
+        {
+            isHover = true;
+            transform.localScale = hoverScale;
+            transform.position = new Vector3(transform.position.x,
+                transform.position.y, hoverZ);
+        }
+        else
+        {
+            isHover = false;
+            transform.localScale = originScale;
+            transform.position = new Vector3(transform.position.x,
+                transform.position.y, originZ);
+        }
+    }
+}
---
+++ WEDO/Assets/MyScript/Person/Person_userimage.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Person_userimage : MonoBehaviour
+{
+
+    public bool isHover = false;
+    public Vector3 originScale;
+    public Vector3 hoverScale;
+    public float scaleRate = 1.1f;
+    public float originZ;
+    public float hoverZ;
+    public static string tempImage = "";
+    public static string chooseUserImage = "";
+
+    // Use this for initialization
+    void Start()
+    {
+        originScale = transform.localScale;
+        hoverScale = scaleRate * originScale;
+        originZ = transform.position.z;
+        hoverZ = originZ - 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkHover();
+        checkClick();
+        checkChoose();
+    }
+
+    private void checkChoose()
+    {
+        if (tempImage.Equals(name))
+        {
+            transform.localScale = hoverScale;
+            transform.position = new Vector3(transform.position.x,
+                transform.position.y, hoverZ);
+        }
+    }
+
+    private void checkClick()
+    {
+        if (isHover)
+        {
+            if (LeftHandProperty.isClosed && !LeftHandProperty.clickUsed)
+            {
+                tempImage = name;
+                LeftHandProperty.clickUsed = true;
+            }
+            if (RightHandProperty.isClosed && !RightHandProperty.clickUsed)
+            {
+                tempImage = name;
+                RightHandProperty.clickUsed = true;
+            }
+        }
+    }
+
+    private void checkHover()
+    {
+        if (RayHit.LeftHitName.Equals(name) || RayHit.RightHitName.Equals(name))
+        {
+            isHover = true;
+            transform.localScale = hoverScale;
+            transform.position = new Vector3(transform.position.x,
+                transform.position.y, hoverZ);
+        }
+        else
+        {
+            isHover = false;
+            transform.localScale = originScale;
+            transform.position = new Vector3(transform.position.x,
+                transform.position.y, originZ);
+        }
+    }
+}
